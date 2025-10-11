45019eb65927b0aa533322b504142d3efd42f250
Sean O'Riordain <seanpor@acm.org>
2025-10-11 15:15:30 +0100

    update email address

--- src/loggrep/__init__.py
+++ src/loggrep/__init__.py
@@ -7,7 +7,7 @@ making it perfect for analyzing logs from specific points in time.
 
 __version__ = "1.0.0"
 __author__ = "Sean"
-__email__ = "seanpor@acm.org"
+__email__ = "seanpor at acm.org"
 __license__ = "Apache-2.0"
 
 from .core import LogSearcher
